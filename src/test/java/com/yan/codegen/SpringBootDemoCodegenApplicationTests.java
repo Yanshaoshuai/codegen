88992cf64c981004eac69a67b76fd50329d5ed03
yanshaoshuai <ahutyss@gmail.com>
2020-08-30 20:45:31 +0800

    first commit

---
+++ src/test/java/com/yan/codegen/SpringBootDemoCodegenApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yan.codegen;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootDemoCodegenApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
